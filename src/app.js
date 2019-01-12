2bec49312c2e40ddd2b28ce2003059180146cbfe
CaityHeath <caity.heath@gmail.com>
2019-01-12 14:50:11 -0800

    fetching from api and rendering hime page, new search page and results

--- src/app.js
+++ src/app.js
@@ -47,16 +47,16 @@ app.use(router);
 app.get('*', handleNotFound)
 
 // HELPER FUNCTIONS
-// function Book(info) {
-//   let placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
+function Book(info) {
+  let placeholderImage = 'https://i.imgur.com/J5LVHEL.jpg';
 
-//   this.title = info.title || 'No title available';
-//   this.author = info.authors[0] || 'No authors available';
-//   this.isbn = info.industryIdentifiers ? `ISBN_13 ${info.industryIdentifiers[0].identifier}` : 'No ISBN available';
-//   this.image_url = info.imageLinks ? info.imageLinks.smallThumbnail : placeholderImage;
-//   this.description = info.description || 'No description available';
-//   this.id = info.industryIdentifiers ? `${info.industryIdentifiers[0].identifier}` : '';
-// }
+  this.title = info.title || 'No title available';
+  this.author = info.authors[0] || 'No authors available';
+  this.isbn = info.industryIdentifiers ? `ISBN_13 ${info.industryIdentifiers[0].identifier}` : 'No ISBN available';
+  this.image_url = info.imageLinks ? info.imageLinks.smallThumbnail : placeholderImage;
+  this.description = info.description || 'No description available';
+  this.id = info.industryIdentifiers ? `${info.industryIdentifiers[0].identifier}` : '';
+}
 
 // Original API Routes
 // app.get('/', getBooks);
